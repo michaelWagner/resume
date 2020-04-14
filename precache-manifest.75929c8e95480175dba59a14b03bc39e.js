149eedb4f68773605a271b0da981b3cec76f0227
Michael Wagner <wagnerdmike@gmail.com>
2020-04-14 03:12:47 -0700

    Updates

---
+++ precache-manifest.75929c8e95480175dba59a14b03bc39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b04de98dda992ce33a420718c80bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bedbbfe55f1b7e9c763",
+    "url": "/static/css/main.7d5217c5.chunk.css"
+  },
+  {
+    "revision": "ff0b834e5a2555056eb2",
+    "url": "/static/js/2.cf765270.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/static/js/2.cf765270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bedbbfe55f1b7e9c763",
+    "url": "/static/js/main.1d6cd78a.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  }
+]);
